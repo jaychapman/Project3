b9be522bab70aae56453ca0c5b3acd6f5472fc52
Jay C <jaychap77@yahoo.com>
2021-01-11 10:47:02 -0500

    deploying

--- server.js
+++ server.js
@@ -35,6 +35,18 @@ app.get("*", (req, res) => {
 //   console.log(`🌎 ==> API server now on port ${PORT}!`);
 // });
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+// mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+// .then(()=> app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
+// .catch((error)=> console.log(error.message));
+
+mongoose.connect(
+  process.env.MONGODB_URI || CONNECTION_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+)
 .then(()=> app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
 .catch((error)=> console.log(error.message));
